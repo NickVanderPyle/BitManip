317cfe605fc2afc69b254347192cf020a626b260
NickVanderPyle <nick.vp@gmail.com>
2013-02-23 15:12:58 -0600

    Added additional test to verify byte to Int32 casting and the treatment of negative shift counts.

--- BitManip.Tests/BitTests.cs
+++ BitManip.Tests/BitTests.cs
@@ -28,7 +28,7 @@ namespace BitManip.Tests
             var valueAtIndex = Bit.GetBitAtIndex(b, index);
             Assert.AreEqual(expectedValue, valueAtIndex);
         }
-        
+
         [TestCase(0, 0, 1, 0)]
         [TestCase(0, 0, 7, 0)]
         [TestCase(0, 2, 3, 0)]
@@ -40,6 +40,26 @@ namespace BitManip.Tests
         [TestCase(1, 0, 3, 1)]
         [TestCase(170, 2, 3, 2)]
         public void NumberFromRange_GivenByte_ReturnsNumberFromRangeOfBits(byte b, int startIndex, int lengthFromStart, int expectedValue)
+        {
+            var valueFromRange = Bit.GetNumberFromBitRange(b, startIndex, lengthFromStart);
+            Assert.AreEqual(expectedValue, valueFromRange);
+        }
+
+        /* GetNumberFromBitRange does a left shift and then a right shift.
+           The first left shift will shift by 8 - (start + length)
+           The second riht shift will shift by 8 - length.
+           When it shifts, the byte is converted to an Int32.
+           Shifting a 32bit number by a shift count will derive the real shift count from the first 5bits.
+           For example:
+           40 << 5000  is the same as  40 << 8
+            5000 == 1001110001000
+            First 5bits...
+            01000 == 8
+        */
+        [TestCase(1, -10, 1, 0)]        // leftshift by 17 then right by 7. Only zero bits remain in first byte.
+        [TestCase(127, 10, 3, 0)]       // leftshift by -5 whose 6 LSB are 27 in positive binary. Left shifts the byte by 27bits leaving only 0s.
+        [TestCase(255, 4000, 3, 7)]     // leftshift by -3995 whose 6 LSB are 5 in positive binary. Then right shift by 5.
+        public void NumberFromRange_AttemptToUseSeeminglyInvalidRange_ReturnsExpectedNumber(byte b, int startIndex, int lengthFromStart, int expectedValue)
         {
             var valueFromRange = Bit.GetNumberFromBitRange(b, startIndex, lengthFromStart);
             Assert.AreEqual(expectedValue, valueFromRange);
